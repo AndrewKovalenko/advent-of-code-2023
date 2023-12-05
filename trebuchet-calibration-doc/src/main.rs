9ce0d8d4fc8e7d8c19b42afd7a9e5244affcd542
Andrew Kovalenko <mr.kovalenko.a.o@gmail.com>
2023-12-04 22:09:23 -0800

    solve the first day puzzle

--- trebuchet-calibration-doc/src/main.rs
+++ trebuchet-calibration-doc/src/main.rs
@@ -10,21 +10,30 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-const NUMBERS: &'static [&str] = &[
-    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+const NUMBERS: &[(&str, &str)] = &[
+    ("zero", "z0o"),
+    ("one", "o1e"),
+    ("two", "t2o"),
+    ("three", "t3e"),
+    ("four", "f4r"),
+    ("five", "f5e"),
+    ("six", "s6x"),
+    ("seven", "s7n"),
+    ("eight", "e8t"),
+    ("nine", "n9e"),
 ];
 
 fn replace_written_digits(input: String) -> String {
     let mut buffer_str = input.clone();
 
-    for (index, number_string) in NUMBERS.iter().enumerate() {
-        buffer_str = buffer_str.replace(number_string, (index + 1).to_string().as_str());
+    for (number_string, replacement) in NUMBERS.iter() {
+        buffer_str = buffer_str.replace(number_string, replacement);
     }
 
     buffer_str
 }
 
-fn extract_number(input: &str) -> u8 {
+fn extract_number(input: String) -> u8 {
     let mut first_digit: Option<u8> = None;
     let mut second_digit: Option<u8> = None;
     let chars: Vec<char> = input.chars().collect();
@@ -51,11 +60,11 @@ fn extract_number(input: &str) -> u8 {
 }
 
 fn main() {
-    let resulted_sum: u128 = read_lines("../../data/input.txt")
+    let resulted_sum: u128 = read_lines("data/input.txt")
         .unwrap()
         .map(|s| replace_written_digits(s.unwrap()))
         .fold(0, |acc, element| {
-            let number_extracted_from_line = extract_number(element.as_str());
+            let number_extracted_from_line = extract_number(element);
             acc + (number_extracted_from_line as u128)
         });
 
@@ -69,12 +78,12 @@ mod test {
     #[test]
     fn test_number_extraction() {
         let input42 = "asdf4sdfg2lkj";
-        let result42 = extract_number(input42);
+        let result42 = extract_number(input42.into());
 
         assert_eq!(result42, 42);
 
         let input55 = "asdf5sdfgolkj";
-        let result55 = extract_number(input55);
+        let result55 = extract_number(input55.into());
 
         assert_eq!(result55, 55);
     }
@@ -84,9 +93,12 @@ mod test {
         let input = "hdmxvkcjdjninebzvmsgsseventhreemhggmhgg11";
         let replaced_digits_string = replace_written_digits(input.into());
 
-        assert_eq!(replaced_digits_string, "hdmxvkcjdj9bzvmsgs73mhggmhgg11");
+        assert_eq!(
+            replaced_digits_string,
+            "hdmxvkcjdjn9ebzvmsgss7nt3emhggmhgg11"
+        );
 
-        let extracted_number = extract_number(&replaced_digits_string.as_str());
+        let extracted_number = extract_number(replaced_digits_string.into());
         assert_eq!(extracted_number, 91);
     }
 }
