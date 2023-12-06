d48ef1ff58f68e54b01e9edb9676d575ae8076fe
Andrew Kovalenko <mr.kovalenko.a.o@gmail.com>
2023-12-05 23:29:16 -0800

    solve first part of day 2

--- game-possibility/src/main.rs
+++ game-possibility/src/main.rs
@@ -1,3 +1,5 @@
+use std::u32;
+
 use data_access::file::read_lines;
 use regex::Regex;
 
@@ -9,6 +11,11 @@ struct CubesMix {
     red_cubes: u32,
     green_cubes: u32,
     blue_cubes: u32,
+}
+
+struct Game {
+    id: u32,
+    draws: Vec<String>,
 }
 
 fn too_many_cubes_in_the_draw(cubes_in_the_bag: u32, draw_line: &String, regex: &Regex) -> bool {
@@ -39,14 +46,21 @@ fn is_game_possible(bag: &CubesMix, draws_data: Vec<String>) -> bool {
     impossible_game_found == None
 }
 
-fn parse_draws(input: String) -> Vec<String> {
+fn parse_draws(input: String) -> Game {
+    let game_id_regex = Regex::new(r"^Game (\d+).*").unwrap();
     let parsed_data: Vec<&str> = input.split(COLON).collect();
-    let game_data = parsed_data[1];
+    let id_part = parsed_data[0];
+    let draw_data = parsed_data[1];
 
-    game_data
+    let draws = draw_data
         .split(SEMICOLON)
         .map(|line| String::from(line))
-        .collect()
+        .collect();
+    let id = game_id_regex.captures(id_part).unwrap()[CAPTURE_GROUP_VALUE_NUMBER]
+        .parse::<u32>()
+        .unwrap();
+
+    Game { draws, id }
 }
 
 fn main() {
@@ -60,13 +74,13 @@ fn main() {
     let number_of_impossible_games = read_lines("data/input.txt") // VsCode debug path
         .unwrap()
         .map(|line| parse_draws(line.unwrap()))
-        .fold(0, |acc, draw_data| {
-            if !is_game_possible(&bag, draw_data) {
-                acc + 1
+        .fold(0, |acc, game_data| {
+            if is_game_possible(&bag, game_data.draws) {
+                acc + game_data.id
             } else {
                 acc
             }
         });
 
-    println!("Number of impossible games: {number_of_impossible_games}")
+    println!("Number of possible games: {number_of_impossible_games}")
 }
