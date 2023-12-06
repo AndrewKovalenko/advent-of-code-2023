a20e2b2b26231d426a31431612b631cd8cfbfca3
Andrew Kovalenko <mr.kovalenko.a.o@gmail.com>
2023-12-05 20:19:34 -0800

    turn file reading function into a reusable crate

--- trebuchet-calibration-doc/src/main.rs
+++ trebuchet-calibration-doc/src/main.rs
@@ -1,14 +1,4 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
-
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
+use data_access::file::read_lines;
 
 const NUMBERS: &[(&str, &str)] = &[
     ("zero", "z0o"),
